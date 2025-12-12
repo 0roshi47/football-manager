db31eb7dcad9c142309304519ae638a623f5fd4c
Pinou <chicmat4@gmail.com>
2025-12-12 11:11:59 +0100

    ajout fenetres, passage accueil en php

---
+++ vue/Accueil.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+  <title>Accueil </title>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <nav  class="navbar">
+        <ul>
+            <li><a href="Accueil.php">Accueil</a></li>
+            <li><a href="GestionRencontre.html">Rencontre</a></li>
+            <li><a href="GestionJoueur.html">Joueurs</a></li>
+            <li><a href="GestionComposition.html">Composition</a></li>
+        </ul>
+    </nav>
+    <h1>Bonjour $user</h1>
+    <div >
+        <table class="tableau">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Heure</th>
+                    <th scope="col">Adversaire</th>
+                    <th scope="col">Lieu</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    <div>
+        <a href="GestionRencontre.html"><button class="button-default">Gérer les matchs</button></a>
+        <a href="GestionJoueur.html"><button class="button-default">Gérer les joueurs</button></a>
+        <a href="GestionComposition.html"><button class="button-default">Gérer les compositions</button></a>
+    </div>
+
+
+</body>
+</html>
+ 
